f984b915e2f92c1c9d8888e2c9ae6879dc0b194e
himanshu-431 <himanshuchaurasia431@gmail.com>
2024-05-13 13:26:45 +0530

    API integration

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -11,8 +11,8 @@ const useMovieTrailer = (movieId) => {
   const getMovieVideos = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/" +
-        movieId +
-        "/videos?language=en-US",
+      movieId +
+      "/videos?language=en-US",
       API_OPTIONS
     );
     const json = await data.json();
